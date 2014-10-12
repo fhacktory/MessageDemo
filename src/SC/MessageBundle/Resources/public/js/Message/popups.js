c5fd4fa02e4e68fee3cf3409bfaaecdcdc4ff7cd
Nicolas Rouvière <nicolasrouviere6@gmail.com>
2014-10-12 09:56:43 +0200

    Cool animations

--- src/SC/MessageBundle/Resources/public/js/Message/popups.js
+++ src/SC/MessageBundle/Resources/public/js/Message/popups.js
@@ -1,6 +1,21 @@
 $(function() {
-    $('body').on('click', '.popup .close', function() {
+    var body = $('body');
+    body.on('click', '.popup .close', function() {
         var id = $(this).attr('data-popup-id');
         $('#'+id).remove();
+    });
+    body.on('click', '.popup .panel-heading', function() {
+        var id = $(this).find('.close').attr('data-popup-id');
+        var element = $('#'+id);
+
+        if(element.hasClass('down')) {
+            element.animate({top:'0px'});
+            element.removeClass('down');
+        }
+        else {
+            element.animate({top:'242px'});
+            element.addClass('down');
+        }
+
     });
 });
